812d10d157946e9a9d5aefb270de76fa5778ca05
Leonardo Oliveira <leo.oliveira8@gmail.com>
2023-11-29 12:59:47 -0300

    add routes

---
+++ golang/projeto/cmd/api/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"github.com/go-chi/chi/v5"
+	"net/http"
+)
+
+func main() {
+	r := chi.NewRouter()
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		param := r.URL.Query().Get("name")
+		msg := "HOME"
+
+		if param != "" {
+			msg = param
+		}
+
+		w.Write([]byte(msg))
+	})
+
+	r.Get("/{productName}", func(w http.ResponseWriter, r *http.Request) {
+		param := chi.URLParam(r, "productName")
+		w.Write([]byte(param))
+	})
+
+	http.ListenAndServe(":3000", r)
+}
