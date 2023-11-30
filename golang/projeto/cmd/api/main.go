440a04f657893fcf0f7e264004f77ee113780e84
Leonardo Oliveira <leo.oliveira8@gmail.com>
2023-11-29 23:36:12 -0300

    add route and tests

--- golang/projeto/cmd/api/main.go
+++ golang/projeto/cmd/api/main.go
@@ -7,11 +7,26 @@ import (
 	"net/http"
 )
 
+type product struct {
+	ID   int
+	Name string
+}
+
+// type myHandler struct{}
+//
+//	func (m myHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+//		w.Write([]byte("myHandler")) //default golang
+//	}
 func main() {
 	r := chi.NewRouter()
+	r.Use(myMiddleware2)
+	r.Use(myMiddleware)
+	//m := myHandler{}
+	//r.Handle("/handler", m)
 
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		param := r.URL.Query().Get("name")
+		println("endpoint")
 		msg := "HOME"
 
 		if param != "" {
@@ -38,5 +53,27 @@ func main() {
 		render.JSON(w, r, obj)
 	})
 
+	r.Post("/product", func(w http.ResponseWriter, r *http.Request) {
+		var product product
+		product.ID = 5
+		render.DecodeJSON(r.Body, &product)
+		render.JSON(w, r, product)
+	})
+
 	http.ListenAndServe(":3000", r)
+}
+
+func myMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		println("before")
+		next.ServeHTTP(w, r)
+		println("after")
+	})
+}
+func myMiddleware2(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		println("request ", r.Method, " url ", r.RequestURI)
+		next.ServeHTTP(w, r)
+		println("after 2")
+	})
 }
