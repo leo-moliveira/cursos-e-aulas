0d174575510542a6cf7e157de02ce10ed9c226ec
Leonardo Oliveira <leo.oliveira8@gmail.com>
2023-12-26 19:14:35 -0300

    add endpoint test

--- golang/projeto/cmd/api/endpoints/handler_error_test.go
+++ golang/projeto/cmd/api/endpoints/handler_error_test.go
@@ -2,6 +2,7 @@ package endpoints
 
 import (
 	"emailn/internal/internalErrors"
+	"encoding/json"
 	"errors"
 	"github.com/stretchr/testify/assert"
 	"net/http"
@@ -35,4 +36,24 @@ func Test_HandlerError_when_endpoint_returns_domain_error(t *testing.T) {
 	handlerFunc.ServeHTTP(res, req)
 	ass.Equal(http.StatusBadRequest, res.Code)
 	ass.Contains(res.Body.String(), "domain error")
+}
+
+func Test_HandlerError_when_endpoint_returns_object_and_status(t *testing.T) {
+	ass := assert.New(t)
+	type bodyForTest struct {
+		Id int
+	}
+	objectExpected := bodyForTest{Id: 2}
+	endpoint := func(w http.ResponseWriter, r *http.Request) (interface{}, int, error) {
+		return objectExpected, 201, nil
+	}
+	handlerFunc := HandlerError(endpoint)
+	req, _ := http.NewRequest("GET", "/", nil)
+	res := httptest.NewRecorder()
+
+	handlerFunc.ServeHTTP(res, req)
+	ass.Equal(http.StatusCreated, res.Code)
+	objectReturned := bodyForTest{}
+	json.Unmarshal(res.Body.Bytes(), &objectReturned)
+	ass.Equal(objectExpected, objectReturned)
 }
