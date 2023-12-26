30113e7562bc83d5197a397f9d9730ef48b8059c
Leonardo Oliveira <leo.oliveira8@gmail.com>
2023-12-26 19:03:45 -0300

    add endpoint test

--- golang/projeto/cmd/api/endpoints/handler_error_test.go
+++ golang/projeto/cmd/api/endpoints/handler_error_test.go
@@ -1,10 +1,23 @@
 package endpoints
 
 import (
+	"emailn/internal/internalErrors"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
 func Test_HandlerError_when_endpoint_returns_internal_error(t *testing.T) {
-	//ass := assert.New(t)s
+	ass := assert.New(t)
+	endpoint := func(w http.ResponseWriter, r *http.Request) (interface{}, int, error) {
+		return nil, 0, internalErrors.ErrInternal
+	}
+	handlerFunc := HandlerError(endpoint)
+	req, _ := http.NewRequest("GET", "/", nil)
+	res := httptest.NewRecorder()
 
+	handlerFunc.ServeHTTP(res, req)
+	ass.Equal(http.StatusInternalServerError, res.Code)
+	ass.Contains(res.Body.String(), internalErrors.ErrInternal.Error())
 }
