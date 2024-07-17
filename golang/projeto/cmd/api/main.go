9dbc63bbbeb26b8eebcf313698766f4b0b4af59d
Leonardo Oliveira <leo.oliveira8@gmail.com>
2024-07-17 12:52:21 -0300

    adiciona parametros rota de teste

--- golang/projeto/cmd/api/main.go
+++ golang/projeto/cmd/api/main.go
@@ -8,7 +8,14 @@ import (
 func main() {
 	r := chi.NewRouter()
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello World"))
+		product := r.URL.Query().Get("product")
+		id := r.URL.Query().Get("id")
+		w.Write([]byte("Hello World " + product + " " + id))
+	})
+
+	r.Get("/{productName}", func(w http.ResponseWriter, r *http.Request) {
+		productName := chi.URLParam(r, "productName")
+		w.Write([]byte(productName))
 	})
 
 	http.ListenAndServe(":3000", r)
