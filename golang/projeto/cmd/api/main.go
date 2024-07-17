625d92a3f71fe1e7c3335150f3614a51c838fc89
Leonardo Oliveira <leo.oliveira8@gmail.com>
2024-07-17 13:05:41 -0300

    adiciona rotas json e json2

--- golang/projeto/cmd/api/main.go
+++ golang/projeto/cmd/api/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/render"
 	"net/http"
 )
 
@@ -16,6 +18,18 @@ func main() {
 	r.Get("/{productName}", func(w http.ResponseWriter, r *http.Request) {
 		productName := chi.URLParam(r, "productName")
 		w.Write([]byte(productName))
+	})
+
+	r.Get("/json", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		obj := map[string]string{"message": "success"}
+		b, _ := json.Marshal(obj)
+		w.Write(b)
+	})
+
+	r.Get("/json2", func(w http.ResponseWriter, r *http.Request) {
+		obj := map[string]string{"message": "success"}
+		render.JSON(w, r, obj)
 	})
 
 	http.ListenAndServe(":3000", r)
