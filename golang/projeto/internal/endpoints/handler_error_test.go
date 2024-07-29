b364ec18abc9d597d317edd34b2fbe610a89b6fa
Leonardo Oliveira <leo.oliveira8@gmail.com>
2024-07-29 12:21:09 -0300

    adiciona test handler error

---
+++ golang/projeto/internal/endpoints/handler_error_test.go
@@ -0,0 +1,24 @@
+package endpoints
+
+import (
+	"emailn/internal"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func Test_HandlerError_when_endpoint_returns_internal_error(t *testing.T) {
+	assert := assert.New(t)
+	endpoint := func(w http.ResponseWriter, r *http.Request) (interface{}, int, error) {
+		return nil, 0, internal.Err
+	}
+	handlerFunc := HandlerError(endpoint)
+	req, _ := http.NewRequest("GET", "/", nil)
+	res := httptest.NewRecorder()
+
+	handlerFunc.ServeHTTP(res, req)
+
+	assert.Equal(http.StatusInternalServerError, res.Code)
+	assert.Contains(res.Body.String(), internal.Err.Error())
+}
