f1e01a82f6f913e057591717deb3f93ae5b29568
Leonardo Oliveira <leo.oliveira8@gmail.com>
2024-07-29 13:02:16 -0300

    adiciona teste handler error obj e status

--- golang/projeto/internal/endpoints/handler_error_test.go
+++ golang/projeto/internal/endpoints/handler_error_test.go
@@ -2,6 +2,7 @@ package endpoints
 
 import (
 	"emailn/internal"
+	"encoding/json"
 	"errors"
 	"github.com/stretchr/testify/assert"
 	"net/http"
@@ -24,7 +25,7 @@ func Test_HandlerError_when_endpoint_returns_internal_error(t *testing.T) {
 	assert.Contains(res.Body.String(), internal.Err.Error())
 }
 
-func Test_HandlerError_when_domain_error(t *testing.T) {
+func Test_HandlerError_when_endpoint_returns_domain_error(t *testing.T) {
 	assert := assert.New(t)
 	endpoint := func(w http.ResponseWriter, r *http.Request) (interface{}, int, error) {
 		return nil, 0, errors.New("domain error")
@@ -37,4 +38,25 @@ func Test_HandlerError_when_domain_error(t *testing.T) {
 
 	assert.Equal(http.StatusBadRequest, res.Code)
 	assert.Contains(res.Body.String(), "domain error")
+}
+
+func Test_HandlerError_when_endpoint_returns_obj_and_status(t *testing.T) {
+	assert := assert.New(t)
+	type bodyForTest struct {
+		Id int
+	}
+	objExpected := bodyForTest{Id: 2}
+	endpoint := func(w http.ResponseWriter, r *http.Request) (interface{}, int, error) {
+		return objExpected, 201, nil
+	}
+	handlerFunc := HandlerError(endpoint)
+	req, _ := http.NewRequest("GET", "/", nil)
+	res := httptest.NewRecorder()
+
+	handlerFunc.ServeHTTP(res, req)
+
+	assert.Equal(http.StatusCreated, res.Code)
+	objReturned := bodyForTest{}
+	json.Unmarshal(res.Body.Bytes(), &objReturned)
+	assert.Equal(objExpected, objReturned)
 }
