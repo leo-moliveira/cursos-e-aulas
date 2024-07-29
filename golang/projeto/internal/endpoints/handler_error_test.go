3afbd20a8f54dd063654b78eebdc41af3d603ca4
Leonardo Oliveira <leo.oliveira8@gmail.com>
2024-07-29 12:56:52 -0300

    adiciona func teste domain error

--- golang/projeto/internal/endpoints/handler_error_test.go
+++ golang/projeto/internal/endpoints/handler_error_test.go
@@ -2,6 +2,7 @@ package endpoints
 
 import (
 	"emailn/internal"
+	"errors"
 	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
@@ -21,4 +22,19 @@ func Test_HandlerError_when_endpoint_returns_internal_error(t *testing.T) {
 
 	assert.Equal(http.StatusInternalServerError, res.Code)
 	assert.Contains(res.Body.String(), internal.Err.Error())
+}
+
+func Test_HandlerError_when_domain_error(t *testing.T) {
+	assert := assert.New(t)
+	endpoint := func(w http.ResponseWriter, r *http.Request) (interface{}, int, error) {
+		return nil, 0, errors.New("domain error")
+	}
+	handlerFunc := HandlerError(endpoint)
+	req, _ := http.NewRequest("GET", "/", nil)
+	res := httptest.NewRecorder()
+
+	handlerFunc.ServeHTTP(res, req)
+
+	assert.Equal(http.StatusBadRequest, res.Code)
+	assert.Contains(res.Body.String(), "domain error")
 }
